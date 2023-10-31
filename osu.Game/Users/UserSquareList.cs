e5d2e8dd3fa0826946a81b0b039e042be03493ac
Joshua Hegedus <josh.hegedus@outlook.com>
2023-10-31 13:53:42 +0100

    added hover user grid panel

--- osu.Game/Users/UserSquareList.cs
+++ osu.Game/Users/UserSquareList.cs
@@ -1,15 +1,17 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Cursor;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Input.Events;
 using osu.Framework.Threading;
 using osu.Game.Graphics;
+using osu.Game.Graphics.Containers;
+using osu.Game.Graphics.Sprites;
 using osu.Game.Online.API.Requests.Responses;
 using osu.Game.Users.Drawables;
 using osuTK;
@@ -18,7 +20,14 @@ namespace osu.Game.Users
 {
     public partial class UserSquareList : VisibilityContainer
     {
+        #region Fields
+        private ScheduledDelegate? popOutDelegate;
+        private List<APIUser>? users;
+        private bool isTargetHovered;
         private readonly UsersList usersList = new UsersList();
+        #endregion
+
+        #region Properties
         public bool IsTargetHovered
         {
             get => isTargetHovered;
@@ -32,6 +41,7 @@ namespace osu.Game.Users
                 }
             }
         }
+
         public List<APIUser>? Users
         {
             get => users;
@@ -41,6 +51,9 @@ namespace osu.Game.Users
                 usersList.UpdateUsers(users ?? new List<APIUser>());
             }
         }
+        #endregion
+
+        #region Protected overrides
         protected override bool OnHover(HoverEvent e)
         {
             schedulePopOut();
@@ -53,16 +66,6 @@ namespace osu.Game.Users
             base.OnHoverLost(e);
         }
 
-        public override void Show()
-        {
-            if (State.Value == Visibility.Visible)
-            {
-                schedulePopOut();
-            }
-
-            base.Show();
-        }
-
         protected override void PopIn()
         {
             ClearTransforms();
@@ -70,10 +73,6 @@ namespace osu.Game.Users
 
             schedulePopOut();
         }
-
-        private ScheduledDelegate? popOutDelegate;
-        private List<APIUser>? users;
-        private bool isTargetHovered;
 
         protected override void PopOut()
         {
@@ -86,7 +85,18 @@ namespace osu.Game.Users
             schedulePopOut();
             return base.OnMouseMove(e);
         }
+        #endregion
 
+        public override void Show()
+        {
+            if (State.Value == Visibility.Visible)
+            {
+                schedulePopOut();
+            }
+            base.Show();
+        }
+
+        #region Private members
         [BackgroundDependencyLoader]
         private void load(OsuColour colours)
         {
@@ -104,14 +114,8 @@ namespace osu.Game.Users
                     Alpha = 0.9f,
                     Colour = colours.Gray3,
                 },
-                usersList
+                usersList,
             });
-        }
-
-        protected override bool OnClick(ClickEvent e)
-        {
-            Console.WriteLine("Container clicked");
-            return base.OnClick(e);
         }
 
         private void schedulePopOut()
@@ -125,12 +129,13 @@ namespace osu.Game.Users
                 }
             }, out popOutDelegate);
         }
+        #endregion
 
         private partial class UsersList : FillFlowContainer
         {
-            public void UpdateUsers(List<APIUser> users)
+            public void UpdateUsers(IReadOnlyCollection<APIUser> users)
             {
-                var avatars = users.Select(u => new UpdateableAvatar(u).With(avatar =>
+                var avatars = users.Select(u => new HoverableUserAvatar(u).With(avatar =>
                 {
                     avatar.Anchor = Anchor.TopLeft;
                     avatar.Origin = Anchor.TopLeft;
@@ -138,8 +143,30 @@ namespace osu.Game.Users
                     avatar.CornerRadius = 5;
                     avatar.Masking = true;
                 })).Take(50).ToArray();
-                Children = avatars;
-                Width = users.Count <= 10 ? users.Count * 33 : 323;
+                var contents = new List<Drawable>();
+                contents.AddRange(avatars);
+
+                if (users.Count > 50)
+                {
+                    contents.Add(new Container()
+                    {
+                        RelativeSizeAxes = Axes.X,
+                        AutoSizeAxes = Axes.Y,
+                        Child = new OsuSpriteText()
+                        {
+                            Anchor = Anchor.TopRight,
+                            Origin = Anchor.TopRight,
+                            Margin = new MarginPadding()
+                            {
+                                Right = 4
+                            },
+                            Text = $"+ {users.Count - 50}"
+                        }
+                    });
+                }
+
+                Children = contents;
+                Width = users.Count <= 10 ? users.Count * 34 : 330;
             }
 
             public UsersList()
@@ -150,7 +177,77 @@ namespace osu.Game.Users
                 AutoSizeAxes = Axes.Y;
                 Direction = FillDirection.Full;
                 Spacing = new Vector2(2, 2);
-                Padding = new MarginPadding(2);
+                Padding = new MarginPadding(2)
+                {
+                    Left = 4,
+                    Right = 4
+                };
+            }
+        }
+
+        private partial class UserGridPanelTooltip : VisibilityContainer, ITooltip<UserGridPanel>
+        {
+            private UserGridPanel? displayedUser;
+
+            protected override void PopIn()
+            {
+                Child = displayedUser;
+                this.FadeIn(20, Easing.OutQuint);
+            }
+            protected override void PopOut() => this.FadeOut(80, Easing.OutQuint);
+
+            public void Move(Vector2 pos) => Position = pos;
+            public void SetContent(UserGridPanel userGridPanel)
+            {
+                if (displayedUser == userGridPanel)
+                {
+                    return;
+                }
+
+                displayedUser = userGridPanel;
+            }
+        }
+
+        private partial class HoverableUserAvatar : OsuClickableContainer, IHasCustomTooltip<UserGridPanel>
+        {
+            public ITooltip<UserGridPanel> GetCustomTooltip() => new UserGridPanelTooltip();
+            public UserGridPanel? TooltipContent => new UserGridPanel(user!) { Width = 300 };
+
+            private readonly APIUser? user;
+
+            [Resolved]
+            private OsuGame? game { get; set; }
+
+            /// <summary>
+            /// A clickable avatar for the specified user, with UI sounds included.
+            /// </summary>
+            /// <param name="user">The user. A null value will get a placeholder avatar.</param>
+            public HoverableUserAvatar(APIUser? user)
+            {
+                this.user = user;
+
+                if (user?.Id != APIUser.SYSTEM_USER_ID)
+                    Action = openProfile;
+            }
+
+            [BackgroundDependencyLoader]
+            private void load()
+            {
+                LoadComponentAsync(new DrawableAvatar(user), Add);
+            }
+
+            private void openProfile()
+            {
+                if (user?.Id > 1 || !string.IsNullOrEmpty(user?.Username))
+                    game?.ShowUser(user);
+            }
+
+            protected override bool OnClick(ClickEvent e)
+            {
+                if (!Enabled.Value)
+                    return false;
+
+                return base.OnClick(e);
             }
         }
     }
